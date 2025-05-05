09cd57b133a7185aaa03f445c5ce2d429d4ba317
Jossany Silva <mjossany@gmail.com>
2025-05-04 21:51:00 -0300

    chore: start lambda implementation

---
+++ aws_go.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	// "github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type AwsGoStackProps struct {
+	awscdk.StackProps
+}
+
+func NewAwsGoStack(scope constructs.Construct, id string, props *AwsGoStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	// The code that defines your stack goes here
+
+	// example resource
+	// queue := awssqs.NewQueue(stack, jsii.String("AwsGoQueue"), &awssqs.QueueProps{
+	// 	VisibilityTimeout: awscdk.Duration_Seconds(jsii.Number(300)),
+	// })
+
+	return stack
+}
+
+func main() {
+	defer jsii.Close()
+
+	app := awscdk.NewApp(nil)
+
+	NewAwsGoStack(app, "AwsGoStack", &AwsGoStackProps{
+		awscdk.StackProps{
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+}
+
+func env() *awscdk.Environment {
+	return nil
+}
---
+++ lambda/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+func HandleRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username cannot be empty")
+	}
+
+	return fmt.Sprintf("Hello, %s!", event.Username), nil
+
+}
+
+func main() {
+
+}
