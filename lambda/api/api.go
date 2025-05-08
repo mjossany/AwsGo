661b7bdbba7b6d0f6f1c5081001dc11e0b372d24
Jossany Silva <mjossany@gmail.com>
2025-05-08 15:18:54 -0300

    chore: implement api handler register user function and create dynamodb resource

--- lambda/api/api.go
+++ lambda/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"lambda-func/database"
+	"lambda-func/types"
 )
 
 type ApiHandler struct {
@@ -12,4 +14,26 @@ func NewApiHandler(dbStore database.DynamoDBClient) ApiHandler {
 	return ApiHandler{
 		dbStore: dbStore,
 	}
+}
+
+func (api ApiHandler) RegisterUserHandler(event types.RegisterUser) error {
+	if event.Username == "" || event.Password == "" {
+		return fmt.Errorf("request has empty parameters")
+	}
+
+	userExists, err := api.dbStore.DoesUserExist(event.Username)
+	if err != nil {
+		return fmt.Errorf("there an error checking if user exists %w", err)
+	}
+
+	if userExists {
+		return fmt.Errorf("a user with that username already exists")
+	}
+
+	err = api.dbStore.InsertUser(event)
+	if err != nil {
+		return fmt.Errorf("error registering the user %w", err)
+	}
+
+	return nil
 }
